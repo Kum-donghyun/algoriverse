e247abd6d48e7d00cabfeb672245cf3220d454bc
Kum-donghyun <kisungkum@gmail.com>
2026-04-06 12:42:43 +0900

    구문 오류 해결

--- front/src/pages/Board/BoardDetail.jsx
+++ front/src/pages/Board/BoardDetail.jsx
@@ -1,7 +1,7 @@
 import React, { useState, useEffect } from 'react';
 import { useParams, useNavigate } from 'react-router-dom';
 import axios from 'axios';
-import '../../styles/board.css';
+import '../../styles/Board.css';
 
 const BoardDetail = () => {
   const { id } = useParams();
